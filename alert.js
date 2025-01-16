d1e24d587dff9a40f9912992a17d0410758a9c93
jeong0300 <dltnwjd8898@naver.com>
2025-01-16 20:48:25 +0900

    Feat: nodeJs 연습

---
+++ alert.js
@@ -0,0 +1,7 @@
+function minus(){
+    alert("마이너스 입니다.");
+}
+
+function plus(){
+    alert("플러스 입니다.");
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express"); // express 모듈 셋팅
+const ejs = require("ejs"); // 페이지 로딩을 위해 필수
+const app = express();
+
+// view 엔진을 ejs를 쓰겠다는 설정
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// 페이지 로딩 함수
+app.get("/", function (req, res){
+    //console.log(res);
+    res.render("test", {}); // view 폴더 밑에 있는 파일을 참조함
+});
+
+app.get("/test", function (req, res){
+    //console.log(res);
+    res.render("test2", {}); // view 폴더 밑에 있는 파일을 참조함
+});
+
+app.get("/main", function (req, res){
+    res.render("main", {});
+});
+
+app.get("/next", function (req, res){
+    res.render("next", {});
+});
+
+app.get("/alert", function (req, res){
+    res.render("alert", {});
+});
+
+// 서버 띄울 대 포트 정보 셋팅 및 처음 실행 시 필요한 기능 수행 가능
+app.listen(3000, function(){
+    console.log("server running");
+});
